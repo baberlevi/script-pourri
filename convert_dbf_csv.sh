70b6d27526887c54de2da002423b0690feeaef39
Levi Baber <baberlevi@gmail.com>
2016-09-09 10:33:31 -0500

    initial commit of hive step

--- convert_dbf_csv.sh
+++ convert_dbf_csv.sh
@@ -14,3 +14,6 @@ mkdir -p csv
 
 #convert the files to csv
 ls -1 dbf | cut -f 1 -d '.' | xargs -I dbffile sh -c 'dbfdump.pl --fs "," dbf/dbffile.dbf > csv/dbffile.csv'
+
+#get column names from dbf
+#dbfdump.pl --info accident.dbf | cut -f 2 | cut -f 1 -d ' ' | tail -n +10 | awk '{print $1" string,"}'
