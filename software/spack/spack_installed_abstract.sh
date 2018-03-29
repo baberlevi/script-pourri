0d068e6047c27ba2f8d647e5d2033e516834e2f0
Levi Baber <baber@iastate.edu>
2018-03-29 17:59:39 -0500

    fix spack_installed scripts

--- software/spack/spack_installed_abstract.sh
+++ software/spack/spack_installed_abstract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for package in $(spack find -l | grep -v - | xargs -n2 echo | tr ' ' ,)
+for package in $(spack find -l | grep -v '\-\-' | xargs -n2 echo | tr ' ' ,)
 do 
     hash=$(echo $package | cut -d ',' -f 1 | grep -i -e [0-9,a-z] ) 
     pkgname=$(echo $package | cut -d ',' -f 2 | cut -d '@' -f 1 | grep -i -e [0-9,a-z] ) 
