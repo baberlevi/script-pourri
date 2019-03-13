bf63411c0c26358f3807d713c2651ac837adf67a
Levi Baber <baberlevi@gmail.com>
2019-03-12 21:55:15 -0500

    works, but slow

--- software/spack/spack_installed_abstract.sh
+++ software/spack/spack_installed_abstract.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-for package in $(spack find -l | grep -v '\-\-' | xargs -n2 echo | tr ' ' ,)
-do 
-    hash=$(echo $package | cut -d ',' -f 1 | grep -i -e [0-9,a-z] ) 
-    pkgname=$(echo $package | cut -d ',' -f 2 | cut -d '@' -f 1 | grep -i -e [0-9,a-z] ) 
-    spack spec /$hash | grep -v -e Input -e ----- | head -n 1 | sed 's/arch.*$//g' | sed 's/%gcc\S*//g' | sed 's/@\S*//g' | sed 's/patches\S*//g' > $pkgname.abstract
+for package in $(spack find --explicit -l | grep -v -e '\-\-' -e 'installed' | xargs -n2 echo | tr ' ' ,)
+do
+    hash=$(echo $package | cut -d ',' -f 1 | grep -i -e [0-9,a-z] )
+    pkgname=$(echo $package | cut -d ',' -f 2 | cut -d '@' -f 1 | grep -i -e [0-9,a-z] )
+    spack spec /$hash | grep -v -e Input -e ----- | head -n 1 | sed 's/arch.*$//g' | sed 's/%gcc\S*//g' | sed 's/@\S*//g' | sed 's/patches\S*//g' > abstracts/$pkgname.$hash
 done
+
