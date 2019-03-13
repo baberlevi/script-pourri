d348a4ab111bebb67f5fe5de06bf836be8f569b2
Levi Baber <baberlevi@gmail.com>
2019-03-12 21:25:27 -0500

    pollock

--- software/spack/spack_installed_abstract.sh
+++ software/spack/spack_installed_abstract.sh
@@ -3,5 +3,5 @@ for package in $(spack find -l | grep -v '\-\-' | xargs -n2 echo | tr ' ' ,)
 do 
     hash=$(echo $package | cut -d ',' -f 1 | grep -i -e [0-9,a-z] ) 
     pkgname=$(echo $package | cut -d ',' -f 2 | cut -d '@' -f 1 | grep -i -e [0-9,a-z] ) 
-    spack spec /$hash | grep -v -e Input -e ----- | sed '/Concretized/,$d' | tr '\n' ' ' | tr -s '\t' ' ' > $pkgname.abstract
+    spack spec /$hash | grep -v -e Input -e ----- | head -n 1 | sed 's/arch.*$//g' | sed 's/%gcc\S*//g' | sed 's/@\S*//g' | sed 's/patches\S*//g'' > $pkgname.abstract
 done
